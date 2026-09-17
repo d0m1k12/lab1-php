d3e1d0f9ac9825534e4f45773593eb0698123194
d0m1k12 <dimmonich2007@gmail.com>
2026-09-17 14:13:45 +0300

    лабораторна 2

--- index.php
+++ index.php
@@ -1,4 +1,27 @@
 <?php
-  echo "<h1>Hello World! Це моя перша PHP-програма. ЙОУ</h1>";
-  phpinfo();
+  $appName = "Task Manager";
+  $taskTitle = "виконати лабораторну роботу з PHP";
+  $taskTimeEstimate = 2;
 ?>
+
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+  <title><?= $appName ?></title>
+</head>
+<body>
+  <header>
+    <h1><?= $appName ?></h1>
+  </header>
+  
+  <main>
+    <h2>Список завдань:</h2>
+    <ul>
+      <li>
+        <strong>Завдання:</strong> <?= $taskTitle ?> <br>
+        <strong>Очікуваний час виконання:</strong> <?= $taskTimeEstimate ?> год.
+      </li>
+    </ul>
+  </main>
+</body>
+</html>
